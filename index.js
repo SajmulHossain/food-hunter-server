ac6748d2d7317ca731d4956317d275daaf94bec8
Sajmul <sajmul1427@gmail.com>
2024-12-24 14:44:18 +0600

    token setup done

--- index.js
+++ index.js
@@ -1,16 +1,86 @@
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-const app = express()
+require("dotenv").config();
+
+const app = express();
 const port = process.env.port || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+  })
+);
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send(`Food server is running`)
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port, ()  => {
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const foodCollection = client.db("foodDB").collection("foods");
+
+    // token related api
+
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.SECRET_KEY, {
+        expiresIn: "2h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true , token});
+    });
+
+    // food get post put
+
+    app.get("/foods", async (req, res) => {
+      const result = await foodCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/foods", async (req, res) => {
+      const data = req.body;
+      const result = await foodCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send(`Food server is running`);
+});
+
+app.listen(port, () => {
   console.log(`Sever is running in in port: ${port}`);
-})
+});
